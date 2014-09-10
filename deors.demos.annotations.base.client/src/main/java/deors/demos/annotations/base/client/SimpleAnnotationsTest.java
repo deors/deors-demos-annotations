8f60bbd1984b698e864dc67a30e13fa22bc966ac
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2014-09-10 20:08:51 +0200

    added Javadocs

--- deors.demos.annotations.base.client/src/main/java/deors/demos/annotations/base/client/SimpleAnnotationsTest.java
+++ deors.demos.annotations.base.client/src/main/java/deors/demos/annotations/base/client/SimpleAnnotationsTest.java
@@ -5,6 +5,12 @@ import deors.demos.annotations.base.Complexity;
 import deors.demos.annotations.base.ComplexityLevel;
 import deors.demos.annotations.base.WorkProduct;
 
+/**
+ * A simple class using Author, Complexity and WorkProduct annotations.
+ *
+ * @author deors
+ * @version 1.0
+ */
 @Author(name = "Albert",
         created = "17/09/2010",
         revision = 3,
@@ -14,6 +20,9 @@ import deors.demos.annotations.base.WorkProduct;
 @Complexity(ComplexityLevel.VERY_SIMPLE)
 public class SimpleAnnotationsTest {
 
+    /**
+     * Default constructor.
+     */
     @Author(name = "Albert",
             created = "17/09/2010") // this annotation types applies also to constructors
                                     // default values assumed for revision and reviewers
@@ -22,6 +31,9 @@ public class SimpleAnnotationsTest {
         super();
     }
 
+    /**
+     * Some method.
+     */
     @Complexity() // this annotation type applies also to methods
                   // the default value 'ComplexityLevel.MEDIUM' is assumed
     public void theMethod() {
---
+++ deors.demos.annotations.base.client/src/main/java/deors/demos/annotations/base/client/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * Package containing a simple class using the annotations defined in
+ * <code>deors.demos.annotations.base</code> module.
+ */
+package deors.demos.annotations.base.client;
