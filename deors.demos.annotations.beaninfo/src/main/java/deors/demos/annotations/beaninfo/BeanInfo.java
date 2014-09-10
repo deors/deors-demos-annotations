b89861b106301af2bd216b98a0e22fedb4c64e4c
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2014-09-10 20:15:29 +0200

    added Javadocs

--- deors.demos.annotations.beaninfo/src/main/java/deors/demos/annotations/beaninfo/BeanInfo.java
+++ deors.demos.annotations.beaninfo/src/main/java/deors/demos/annotations/beaninfo/BeanInfo.java
@@ -6,6 +6,13 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * The BeanInfo annotation type, used to capture descriptive information of the bean
+ * to be latter used to generate BeanInfo types suitable for visual editors.
+ *
+ * @author deors
+ * @version 1.0
+ */
 @Documented
 @Target({
     ElementType.TYPE,
@@ -16,8 +23,15 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.SOURCE)
 public @interface BeanInfo {
 
+    /** The element description. */
     String description() default "";
+
+    /** Whether the element is an expert feature. */
     boolean expert() default false;
+
+    /** Whether the element should be hidden in menus of visual editors. */
     boolean hidden() default false;
+
+    /** Whether the element should be prioritized in menus of visual editors. */
     boolean preferred() default false;
 }
---
+++ deors.demos.annotations.beaninfo/src/main/java/deors/demos/annotations/beaninfo/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Package containing the BeanInfo annotation type.
+ */
+package deors.demos.annotations.beaninfo;
