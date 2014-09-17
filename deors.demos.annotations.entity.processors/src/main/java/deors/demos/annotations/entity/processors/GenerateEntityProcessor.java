31b18851e5de916b709124cc44e02263dcd79b29
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2014-09-17 23:26:11 +0200

    Refactored to improve legibility of generation logic

--- deors.demos.annotations.entity.processors/src/main/java/deors/demos/annotations/entity/processors/GenerateEntityProcessor.java
+++ deors.demos.annotations.entity.processors/src/main/java/deors/demos/annotations/entity/processors/GenerateEntityProcessor.java
@@ -47,6 +47,27 @@ public class GenerateEntityProcessor
     /** String used to append to an entity name when creating the implementation. */
     private static final String IMPL = "Impl";
 
+    /** Package name of the class to generate. */
+    private String packageName = "";
+
+    /** Entity name. */
+    private String entityName = "";
+
+    /** Name of the Entity implementation class. */
+    private String implName = "";
+
+    /** Qualified name of the Entity implementation class. */
+    private String qualifiedName = "";
+
+    /** The field list. */
+    private List<String> fieldNames = new ArrayList<>();
+
+    /** Map containing each field type. */
+    private Map<String, String> fieldTypes = new HashMap<>();
+
+    /** Map containing whether a field is part of the entity key. */
+    private Map<String, Boolean> fieldId = new HashMap<>();
+
     /**
      * Default constructor.
      */
@@ -71,116 +92,141 @@ public class GenerateEntityProcessor
             return true;
         }
 
-        try {
-            String packageName = "";
-            String entityName = "";
-            String implName = "";
-            String qualifiedName = "";
-            List<String> fieldNames = new ArrayList<>();
-            Map<String, String> fieldTypes = new HashMap<>();
-            Map<String, Boolean> fieldId = new HashMap<>();
+        for (Element e : roundEnv.getElementsAnnotatedWith(GenerateEntity.class)) {
 
-            for (Element e : roundEnv.getElementsAnnotatedWith(GenerateEntity.class)) {
+            if (e.getKind() != ElementKind.INTERFACE) {
+                continue;
+            }
 
-                if (e.getKind() == ElementKind.INTERFACE) {
+            TypeElement interfaceElement = (TypeElement) e;
+            prepareEntityData(interfaceElement);
 
-                    TypeElement interfaceElement = (TypeElement) e;
-                    PackageElement packageElement = (PackageElement) interfaceElement.getEnclosingElement();
+            for (Element interfaceMember : interfaceElement.getEnclosedElements()) {
 
-                    packageName = packageElement.getQualifiedName().toString();
-                    entityName = interfaceElement.getSimpleName().toString();
-                    implName = entityName + IMPL;
-                    qualifiedName = interfaceElement.getQualifiedName().toString() + IMPL;
-
-                    processingEnv.getMessager().printMessage(
-                        Diagnostic.Kind.NOTE,
-                        "annotated interface: " + entityName, e);
-
-                    for (Element member : interfaceElement.getEnclosedElements()) {
-
-                        if (!(member instanceof ExecutableElement)) {
-                            continue;
-                        }
-
-                        ExecutableElement method = (ExecutableElement) member;
-                        GenerateEntity annotation = method.getAnnotation(GenerateEntity.class);
-
-                        String fieldName = "";
-                        String methodName = method.getSimpleName().toString();
-                        if (methodName.startsWith("get")
-                            || methodName.startsWith("set")) {
-                            fieldName = methodName.substring(3).toLowerCase();
-                        } else if (methodName.startsWith("is")) {
-                            fieldName = methodName.substring(2).toLowerCase();
-                        } else {
-                            continue;
-                        }
-
-                        String fieldType = "";
-                        // if getter we know the field type is the return type
-                        // if not, look for first parameter
-                        if (method.getReturnType().equals(void.class)) {
-                            if (method.getParameters().isEmpty()) {
-                                continue;
-                            }
-                            fieldType = method.getParameters().get(0).asType().toString();
-                        } else {
-                            fieldType = method.getReturnType().toString();
-                        }
-
-                        boolean id = false;
-                        if (annotation != null) {
-                            id = annotation.id();
-                        }
-
-                        if (!fieldNames.contains(fieldName)) {
-                            fieldNames.add(fieldName);
-                            fieldTypes.put(fieldName, fieldType);
-                            fieldId.put(fieldName, id);
-                        }
-                    }
+                if (!(interfaceMember instanceof ExecutableElement)) {
+                    continue;
                 }
+
+                ExecutableElement methodElement = (ExecutableElement) interfaceMember;
+                prepareEntityType(methodElement);
             }
+        }
 
-            if (!entityName.isEmpty()) {
+        if (!entityName.isEmpty()) {
+            generateEntityClass();
+        }
 
-                Properties props = new Properties();
-                URL url = this.getClass().getClassLoader().getResource("velocity.properties");
-                props.load(url.openStream());
+        return true;
+    }
 
-                VelocityEngine ve = new VelocityEngine(props);
-                ve.init();
+    /**
+     * Prepares Entity data from the information available in the
+     * interface and annotation.
+     *
+     * @param interfaceElement element describing the interface
+     */
+    private void prepareEntityData(TypeElement interfaceElement) {
 
-                VelocityContext vc = new VelocityContext();
+        PackageElement packageElement = (PackageElement) interfaceElement.getEnclosingElement();
 
-                vc.put("packageName", packageName);
-                vc.put("entityName", entityName);
-                vc.put("implName", implName);
-                vc.put("fieldNames", fieldNames);
-                vc.put("fieldTypes", fieldTypes);
-                vc.put("fieldId", fieldId);
+        packageName = packageElement.getQualifiedName().toString();
+        entityName = interfaceElement.getSimpleName().toString();
+        implName = entityName + IMPL;
+        qualifiedName = interfaceElement.getQualifiedName().toString() + IMPL;
 
-                // adding DisplayTool from Velocity Tools library
-                vc.put("display", new DisplayTool());
+        processingEnv.getMessager().printMessage(
+            Diagnostic.Kind.NOTE,
+            "annotated interface: " + entityName, interfaceElement);
+    }
 
-                Template vt = ve.getTemplate("entity.vm");
+    /**
+     * Prepares Entity type from the information of methods in the
+     * interface and annotations.
+     *
+     * @param methodElement element describing the method
+     */
+    private void prepareEntityType(ExecutableElement methodElement) {
 
-                JavaFileObject jfo = processingEnv.getFiler().createSourceFile(qualifiedName);
+        GenerateEntity annotation = methodElement.getAnnotation(GenerateEntity.class);
 
-                processingEnv.getMessager().printMessage(
-                    Diagnostic.Kind.NOTE,
-                    "creating source file: " + jfo.toUri());
+        String fieldName = "";
+        String methodName = methodElement.getSimpleName().toString();
+        if (methodName.startsWith("get")
+            || methodName.startsWith("set")) {
+            fieldName = methodName.substring(3).toLowerCase();
+        } else if (methodName.startsWith("is")) {
+            fieldName = methodName.substring(2).toLowerCase();
+        } else {
+            return;
+        }
 
-                Writer writer = jfo.openWriter();
-
-                processingEnv.getMessager().printMessage(
-                    Diagnostic.Kind.NOTE,
-                    "applying velocity template: " + vt.getName());
-
-                vt.merge(vc, writer);
-
-                writer.close();
+        String fieldType = "";
+        // if getter we know the field type is the return type
+        // if not, look for first parameter
+        if (methodElement.getReturnType().equals(void.class)) {
+            if (methodElement.getParameters().isEmpty()) {
+                return;
             }
+            fieldType = methodElement.getParameters().get(0).asType().toString();
+        } else {
+            fieldType = methodElement.getReturnType().toString();
+        }
+
+        boolean id = false;
+        if (annotation != null) {
+            id = annotation.id();
+        }
+
+        if (!fieldNames.contains(fieldName)) {
+            fieldNames.add(fieldName);
+            fieldTypes.put(fieldName, fieldType);
+            fieldId.put(fieldName, id);
+        }
+    }
+
+    /**
+     * Generates the Entity class using Apache Velocity templates.
+     */
+    private void generateEntityClass() {
+
+        try {
+            Properties props = new Properties();
+            URL url = this.getClass().getClassLoader().getResource("velocity.properties");
+            props.load(url.openStream());
+
+            VelocityEngine ve = new VelocityEngine(props);
+            ve.init();
+
+            VelocityContext vc = new VelocityContext();
+
+            vc.put("packageName", packageName);
+            vc.put("entityName", entityName);
+            vc.put("implName", implName);
+            vc.put("fieldNames", fieldNames);
+            vc.put("fieldTypes", fieldTypes);
+            vc.put("fieldId", fieldId);
+
+            // adding DisplayTool from Velocity Tools library
+            vc.put("display", new DisplayTool());
+
+            Template vt = ve.getTemplate("entity.vm");
+
+            JavaFileObject jfo = processingEnv.getFiler().createSourceFile(qualifiedName);
+
+            processingEnv.getMessager().printMessage(
+                Diagnostic.Kind.NOTE,
+                "creating source file: " + jfo.toUri());
+
+            Writer writer = jfo.openWriter();
+
+            processingEnv.getMessager().printMessage(
+                Diagnostic.Kind.NOTE,
+                "applying velocity template: " + vt.getName());
+
+            vt.merge(vc, writer);
+
+            writer.close();
+
         } catch (ResourceNotFoundException rnfe) {
             processingEnv.getMessager().printMessage(
                 Diagnostic.Kind.ERROR,
@@ -198,7 +244,5 @@ public class GenerateEntityProcessor
                 Diagnostic.Kind.ERROR,
                 e.getLocalizedMessage());
         }
-
-        return true;
     }
 }
