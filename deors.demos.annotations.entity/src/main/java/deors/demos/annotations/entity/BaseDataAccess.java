7e5c25c66f8ce6440509cc853bf20d2e1e1935a1
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2014-10-03 00:03:46 +0200

    added missing Javadocs

--- deors.demos.annotations.entity/src/main/java/deors/demos/annotations/entity/BaseDataAccess.java
+++ deors.demos.annotations.entity/src/main/java/deors/demos/annotations/entity/BaseDataAccess.java
@@ -1,9 +1,41 @@
 package deors.demos.annotations.entity;
 
+/**
+ * A generic base data access interface, parameterizing the type
+ * of the entity and the type of the key.
+ *
+ * @author deors
+ * @version 1.0
+ */
 public interface BaseDataAccess<T, K> {
 
+    /**
+     * Selects a bean by its id.
+     *
+     * @param id the id of the bean to be searched for in the data store
+     * @return the found bean
+     */
     T selectById(K id);
+
+    /**
+     * Inserts a new bean in the data store.
+     *
+     * @param newRecord the bean to be inserted in the data store
+     * @return the bean including any enhancement or persistencia information (if any)
+     */
     T insert(T newRecord);
+
+    /**
+     * Updates an existing bean in the data store.
+     *
+     * @param updatedRecord the bean to be updated in the data store
+     */
     void update(T updatedRecord);
+
+    /**
+     * Deletes an existing bean in the data store.
+     *
+     * @param deletedRecord the bean to be deleted in the data store
+     */
     void delete(T deletedRecord);
 }
