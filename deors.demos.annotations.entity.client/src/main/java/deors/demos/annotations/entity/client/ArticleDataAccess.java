ecbabb2a4a4dd0f26fdfab25d271fd7c8ed30219
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2014-10-03 00:07:17 +0200

    added missing Javadocs

--- deors.demos.annotations.entity.client/src/main/java/deors/demos/annotations/entity/client/ArticleDataAccess.java
+++ deors.demos.annotations.entity.client/src/main/java/deors/demos/annotations/entity/client/ArticleDataAccess.java
@@ -5,10 +5,23 @@ import java.util.List;
 import deors.demos.annotations.entity.BaseDataAccess;
 import deors.demos.annotations.entity.GenerateDataAccess;
 
+/**
+ * A data access interface for the Article bean.
+ *
+ * @author deors
+ * @version 1.0
+ */
 @GenerateDataAccess
 public interface ArticleDataAccess
     extends BaseDataAccess<Article, String> {
 
+    /**
+     * A custom query that uses two parameters as search criteria.
+     *
+     * @param criteria1 the first search criteria
+     * @param criteria2 the second search criteria
+     * @return
+     */
     @GenerateDataAccess("select custom query clauses")
     List<Article> getByCustomCriteria(String criteria1, String criteria2);
 }
