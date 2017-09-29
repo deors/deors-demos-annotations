97202ee4e8446939f647e04577b8e2aaee5d2408
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2017-09-29 10:33:40 +0200

    updated root folder in batch scripts

--- deors.demos.annotations.beaninfo.client/build-with-javac.bat
+++ deors.demos.annotations.beaninfo.client/build-with-javac.bat
@@ -1,7 +1,8 @@
 @echo off
 set OPT=-XprintProcessorInfo -XprintRounds
-set CP=c:\projects\deors.demos\annotations\deors.demos.annotations.beaninfo\target\deors.demos.annotations.beaninfo-1.0-SNAPSHOT.jar
-set PROC=%CP%;c:\projects\deors.demos\annotations\deors.demos.annotations.beaninfo.processors\target\deors.demos.annotations.beaninfo.processors-1.0-SNAPSHOT.jar
+set ROOT_FOLDER=c:\code
+set CP=%ROOT_FOLDER%\deors.demos\annotations\deors.demos.annotations.beaninfo\target\deors.demos.annotations.beaninfo-1.0-SNAPSHOT.jar
+set PROC=%CP%;%ROOT_FOLDER%\deors.demos\annotations\deors.demos.annotations.beaninfo.processors\target\deors.demos.annotations.beaninfo.processors-1.0-SNAPSHOT.jar
 set SRC=src\main\java\deors\demos\annotations\beaninfo\client\Article.java
 set GEN_SRC=target\generated-sources\annotations
 set GEN_CLS=target\classes
--- deors.demos.annotations.entity.client/build-with-javac.bat
+++ deors.demos.annotations.entity.client/build-with-javac.bat
@@ -1,7 +1,8 @@
 @echo off
 set OPT=-XprintProcessorInfo -XprintRounds
-set CP=c:\projects\deors.demos\annotations\deors.demos.annotations.entity\target\deors.demos.annotations.entity-1.0-SNAPSHOT.jar
-set PROC=%CP%;c:\projects\deors.demos\annotations\deors.demos.annotations.entity.processors\target\deors.demos.annotations.entity.processors-1.0SNAPSHOT.jar
+set ROOT_FOLDER=c:\code
+set CP=%ROOT_FOLDER%\deors.demos\annotations\deors.demos.annotations.entity\target\deors.demos.annotations.entity-1.0-SNAPSHOT.jar
+set PROC=%CP%;%ROOT_FOLDER%\deors.demos\annotations\deors.demos.annotations.entity.processors\target\deors.demos.annotations.entity.processors-1.0SNAPSHOT.jar
 set PROC=%PROC%;%HOME%\.m2\repository\org\apache\velocity\velocity\1.6.4\velocity-1.6.4.jar
 set PROC=%PROC%;%HOME%\.m2\repository\commons-collections\commons-collections\3.2.1\commons-collections-3.2.1.jar
 set PROC=%PROC%;%HOME%\.m2\repository\commons-lang\commons-lang\2.4\commons-lang-2.4.jar
